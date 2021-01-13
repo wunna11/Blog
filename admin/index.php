0927718297cfc083a0193939debb8ee694327166
wunna11 <wana192770@gmail.com>
2021-01-13 16:23:43 +0630

    added localbranch

--- admin/index.php
+++ admin/index.php
@@ -6,9 +6,24 @@
     header('location: login.php');
   }
 
+  if(!empty($_GET['pageno'])) {
+    $pageno = $_GET['pageno'];
+  } else {
+    $pageno = 1;
+  }
+  $numOfrecs = 2;
+  $offset = ($pageno - 1) * $numOfrecs;
+
+
   $stmt = $pdo->prepare("SELECT * FROM posts ORDER BY id DESC");
   $stmt->execute();
+  $rawresult = $stmt->fetchAll();
+  $total_pages = ceil(count($rawresult) / $numOfrecs);
+
+  $stmt = $pdo->prepare("SELECT * FROM posts ORDER BY id DESC LIMIT $offset,$numOfrecs");
+  $stmt->execute();
   $result = $stmt->fetchAll();
+
 ?>
 
 
@@ -60,9 +75,24 @@
                   <?php else: ?>
                   <?php endif; ?>  
                   </tbody>
-                </table>
+                </table><br>
+                  <nav aria-label="Page navigation example" style="float:right">
+                  <ul class="pagination">
+                    <li class="page-item"><a class="page-link" href="?pageno=1">First</a></li>
+                    <li class="page-item <?php if($pageno <= 1) {echo 'disabled';} ?>">
+                      <a class="page-link" href="<?php if($pageno <= 1) {echo '#';} else{echo '?pageno='.($pageno-1);} ?>">Previous</a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#"><?php echo $pageno; ?></a></li>
+                    <li class="page-item <?php if($pageno >= $total_pages) {echo 'disabled';} ?>">
+                      <a class="page-link" href="<?php if($pageno >= $total_pages) {echo '#';} else{echo '?pageno='.($pageno+1);} ?>">Next</a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="?pageno=<?php echo $total_pages; ?>">Last</a></li>
+                  </ul>
+                </nav>
+
               </div>
               <!-- /.card-body -->
+              
               
             </div>
             <!-- /.card -->
