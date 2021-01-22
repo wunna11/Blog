c5a668393f97f4d5e92f7c634467c6e98119ce6f
wunna11 <wana192770@gmail.com>
2021-01-23 00:57:23 +0630

    pdo error

--- config/config.php
+++ config/config.php
@@ -1,7 +1,16 @@
 <?php
-    $options = array(
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    );
+    // $options = array(
+    //     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    // );
 
-    $pdo = new PDO("mysql:dbname=blogapp;host=localhost",'root','');
+    // $pdo = new PDO("mysql:dbname=blogapp;host=localhost",'root','');
+
+    try {
+        $pdo = new PDO("mysql:dbname=blogapp;host=localhost", 'root', '');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PODException $e) {
+            die($e->getMessage());
+        } catch(Excpetion $e) {
+            die($e->getMessage());
+        }  
 ?>
